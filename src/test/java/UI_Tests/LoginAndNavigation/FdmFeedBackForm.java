f84d8e6059fa3d7ed5842608b571f44fee09f103
d.kuzmichev <komradekuzmi4ev@gmail.com>
2023-04-06 15:17:12 +0300

    Переписал на 1 метод - 1 проверка

--- src/test/java/UI_Tests/LoginAndNavigation/FdmFeedBackForm.java
+++ src/test/java/UI_Tests/LoginAndNavigation/FdmFeedBackForm.java
@@ -2,6 +2,7 @@ package UI_Tests.LoginAndNavigation;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.testng.Assert;
 
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -11,21 +12,43 @@ public class FdmFeedBackForm {
     private static final String TEST_NUMBER = "9950378814";
     private static final String TEST_TEXT = "Произвольный текст - тестирование формы";
 
-    private final SelenideElement inputName = $x("/html/body/div[5]/div/div[6]/div/form/div[2]/input[1]");
-    private final SelenideElement inputNumber = $x("/html/body/div[5]/div/div[6]/div/form/div[2]/input[2]");
-    private final SelenideElement inputText = $x("/html/body/div[5]/div/div[6]/div/form/div[3]/textarea");
-    private final SelenideElement submitBtn = $x("/html/body/div[5]/div/div[6]/div/form/div[4]/button");
+    private final SelenideElement scroll = $x("//button[(@class =\"send-btn\")]");
+    private final SelenideElement inputName = $x("//input[(@name=\"name\")]");
+    private final SelenideElement inputNumber = $x("//input[(@name=\"tel\")]");
+    private final SelenideElement inputText = $x("//textarea[(@name=\"message\")]");
+    private final SelenideElement submitBtn = $x("//button[(@class=\"send-btn\")]");
+    private final SelenideElement expected = $x("//div[(@class=\"success-message\")]");
+    private final SelenideElement actual = $x("//div[(@class=\"success-message\")]");
 
 
-    public void fdmFeedBackForm() {
-
-        inputName.shouldBe(Condition.enabled).sendKeys(TEST_NAME);
-        inputNumber.shouldBe(Condition.enabled).sendKeys(TEST_NUMBER);
-        inputText.shouldBe(Condition.enabled).sendKeys(TEST_TEXT);
-        submitBtn.shouldBe(Condition.enabled).click();
-
-        $x("/html/body/div[5]/div/div[6]/div/form/div[5]/div[1]")
-                .shouldHave(Condition.exactText("Сообщение отправлено!"));
-
+    /*Скролл к форме*/
+    public void scrollToForm() {
+        scroll.shouldBe(Condition.enabled).scrollTo();
     }
+
+    /*Ввод имени*/
+    public void insertName() {
+        inputName.shouldBe(Condition.enabled).sendKeys(TEST_NAME);
+    }
+
+    /*Ввод номера*/
+    public void insertNumberForm() {
+        inputNumber.shouldBe(Condition.enabled).sendKeys(TEST_NUMBER);
+    }
+
+    /*Ввод текста*/
+    public void insertText() {
+        inputText.shouldBe(Condition.enabled).sendKeys(TEST_TEXT);
+    }
+
+    /*Клик по кнопке - отправить сообщение*/
+    public void clickSubmitBtn() {
+        submitBtn.shouldBe(Condition.enabled).click();
+    }
+
+    /*Проверка отправки сообщения*/
+    public void checkedMessage() {
+        actual.shouldHave(Condition.text("Сообщение отправлено!"));
+    }
+
 }
---
+++ src/test/java/UI_Tests/test/FdmFeedBackFormTest.java
@@ -0,0 +1,29 @@
+package UI_Tests.test;
+
+import FrameWork.DriversConfiguration.ConfigurationForTest;
+import UI_Tests.LoginAndNavigation.FdmFeedBackForm;
+import UI_Tests.LoginAndNavigation.FdmLoginPage;
+import org.testng.annotations.Test;
+
+public class FdmFeedBackFormTest extends ConfigurationForTest {
+
+
+    @Test(testName = "Тест - Проверка формы обратной связи", priority = 1)
+    public void logInTest() {
+        FdmLoginPage fdmLoginPage = new FdmLoginPage();
+        fdmLoginPage.applyCity();
+        fdmLoginPage.clickEnter();
+        fdmLoginPage.insertNumber();
+        fdmLoginPage.clickEntering();
+        fdmLoginPage.insertCode();
+        fdmLoginPage.clickMainEnter();
+
+        FdmFeedBackForm fdmFeedBackForm = new FdmFeedBackForm();
+        fdmFeedBackForm.scrollToForm();
+        fdmFeedBackForm.insertName();
+        fdmFeedBackForm.insertNumberForm();
+        fdmFeedBackForm.insertText();
+        fdmFeedBackForm.clickSubmitBtn();
+        fdmFeedBackForm.checkedMessage();
+    }
+}

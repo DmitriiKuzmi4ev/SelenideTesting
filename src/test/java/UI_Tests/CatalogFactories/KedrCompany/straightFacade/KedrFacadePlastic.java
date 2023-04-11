548acf00afba44f0c3a79cb73d172bc5ea2da166
d.kuzmichev <komradekuzmi4ev@gmail.com>
2023-04-11 13:00:16 +0300

    Правки

--- src/test/java/UI_Tests/CatalogFactories/KedrCompany/straightFacade/KedrFacadePlastic.java
+++ src/test/java/UI_Tests/CatalogFactories/KedrCompany/straightFacade/KedrFacadePlastic.java
@@ -14,23 +14,24 @@ public class KedrFacadePlastic {
 
     private final SelenideElement catalog = $x("//a[(@class=\"header__link nav__link nav__link--arrow \")]");
     private final SelenideElement kedr = $x("//a[contains(@href, \"/main/factory/select-factory/kedr/\")]");
-    private final SelenideElement plastic = $x("//a[contains(@href, \"/select-facade/plastik/\")]");
-    private final SelenideElement facadeWithOutEdge = $x("/html/body/div[5]/div/div[2]/div[2]/div/div[1]/div/div[1]/a/span");
+    private final SelenideElement plastic = $x("//a[(@href=\"/select-facade/plastik/\")]");
+    private final SelenideElement facadeWithOutEdge = $x("//a[(@href=\"/catalog/decors/simple\")]");
     private final ElementsCollection decorCollection = $$x("//div[contains(@class, \"col-md-6 col-lg-3 mb-4 decor-item\")]");
-    private final SelenideElement continue1 = $x("//*[@id=\"exampleModal\"]/div/form/div/div[3]/button[2]");
+    private final SelenideElement continue1 = $x("//div[(@class=\"modal-content\")]//ancestor::button[(@class=\"btn btn-primary\")]");
     private final SelenideElement heightArea = $x("//input[contains(@class, \"detail-height-input form-control\")]");
     private final SelenideElement widthArea = $x("//input[contains(@class, \"detail-width-input form-control\")]");
-    private final SelenideElement calculate = $x("//button[contains(@class, \"btn btn-success mb-4\")]");
-    private final SelenideElement scroll1 = $x("//a[contains(@data-href, \"/order/add-to-basket/\")]");
-    private final SelenideElement scroll2 = $x("//a[contains(@data-type, \"go-to-checkout\")]");
-    private final SelenideElement addToBasket = $x("//a[(@data-href=\"/order/add-to-basket/\")]");
+    private final SelenideElement calculate = $x("//div[(@id=\"calculateButtonBlock\")]//ancestor::button[(@id=\"submitButton\")]");
+    private final SelenideElement addToBasket = $x("//div[(@class=\"card-body\")]//ancestor::a[(@data-href=\"/order/add-to-basket/\")]");
     private final SelenideElement goToBasket = $x("//div[(@class=\"card-body\")]//ancestor::a[(@href=\"/main/basket/\")]");
-    private final SelenideElement goToCheckOut = $x("//a[(@class=\"btn btn-lg btn-success w-100 mb-4 cart-payment\")]");
+    private final SelenideElement goToCheckOut = $x("//a[(@data-type=\"go-to-checkout\")]");
     private final SelenideElement qrCode = $x("//input[(@value=\"raiff\")]//ancestor::label");
     private final SelenideElement qrCodeRadio = $x("//input[(@value=\"raiff\")]");
     private final SelenideElement deliveryCity = $x("//label[(@for=\"deliveryCityCheckbox\")]");
     private final SelenideElement deliveryCityCheckbox = $x("//input[(@id=\"deliveryCityCheckbox\")]");
     private final SelenideElement paymentBtn = $x("//button[contains(@class, \" btn-success w-100\")]");
+
+    private final SelenideElement scroll1 = $x("//div[(@class=\"card-body\")]//ancestor::a[(@data-href=\"/order/add-to-basket/\")]");
+    private final SelenideElement scroll2 = $x("//div[(@class=\"card-body\")]//ancestor::a[(@data-type=\"go-to-checkout\")]");
 
 
     /*Каталог*/
@@ -61,17 +62,17 @@ public class KedrFacadePlastic {
 
     /*Продолжить*/
     public void continue1CLick() {
-        continue1.shouldBe(Condition.enabled).click();
+        continue1.shouldBe(Condition.exist).click();
     }
 
     /*Высота*/
     public void heightAreaInsert() {
-        heightArea.shouldBe(Condition.enabled).sendKeys(BASE_HEIGHT);
+        heightArea.shouldBe(Condition.editable).sendKeys(BASE_HEIGHT);
     }
 
     /*Ширина*/
     public void widthAreaInsert() {
-        widthArea.shouldBe(Condition.enabled).sendKeys(BASE_WIDTH);
+        widthArea.shouldBe(Condition.editable).sendKeys(BASE_WIDTH);
     }
 
     /*Рассчитать стоимость заказа*/
@@ -81,7 +82,8 @@ public class KedrFacadePlastic {
 
     /*Скролл*/
     public void scroll1Do() {
-        scroll1.scrollTo();
+        scroll1.shouldBe(Condition.visible);
+        Selenide.executeJavaScript("arguments[0].scrollIntoView(true)", scroll1);
     }
 
     /*Добавить в корзину*/
@@ -96,7 +98,8 @@ public class KedrFacadePlastic {
 
     /*Скроллим до - перейти к оформлению*/
     public void scroll2Do() {
-        scroll2.scrollTo();
+        scroll2.shouldBe(Condition.visible);
+        Selenide.executeJavaScript("arguments[0].scrollIntoView(true)", scroll2);
     }
 
     /*Перейти к оформлению*/
